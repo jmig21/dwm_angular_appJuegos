5a3dc3ca901b3a17e0cf8deca5070283f23f5905
Manuel <88690070+jmig21@users.noreply.github.com>
2023-09-25 20:34:29 -0300

    add header component

--- angular_JuegosOnline/src/app/app.module.ts
+++ angular_JuegosOnline/src/app/app.module.ts
@@ -5,13 +5,15 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { GamesListComponent } from './games-list/games-list.component';
 import { GameComponent } from './game/game.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
 
   declarations: [
     AppComponent,
     GamesListComponent,
-    GameComponent
+    GameComponent,
+    HeaderComponent
   ],
   imports: [
     HttpClientModule,
