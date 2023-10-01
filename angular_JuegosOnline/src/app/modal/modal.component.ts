49493496229f1adabdaa5742039551c098d9dbaa
Manuel <88690070+jmig21@users.noreply.github.com>
2023-09-30 22:48:57 -0300

    definiendo el modal component

--- angular_JuegosOnline/src/app/modal/modal.component.ts
+++ angular_JuegosOnline/src/app/modal/modal.component.ts
@@ -1,5 +1,5 @@
-import { Component, ViewChild } from '@angular/core';
-import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+// modal.component.ts
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-modal',
@@ -7,11 +7,17 @@ import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
   styleUrls: ['./modal.component.css']
 })
 export class ModalComponent {
-  @ViewChild('myModal') modalContent!: any; // ViewChild para obtener una referencia al contenido del modal
+  on() {
+    const overlay = document.getElementById("overlay");
+    if (overlay) {
+      overlay.style.display = "block";
+    }
+  }
 
-  constructor(private modalService: NgbModal) { }
-
-  mostrarModal() {
-    this.modalService.open(this.modalContent, { centered: true });
+  off() {
+    const overlay = document.getElementById("overlay");
+    if (overlay) {
+      overlay.style.display = "none";
+    }
   }
 }
